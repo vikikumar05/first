b41208cd3989de499a7b1c8c70d53270def0c613
Viki Kumar <vikikumar0505@gmail.com>
2025-06-19 15:19:36 +0530

    Changes in Navbar CSS

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,11 +5,12 @@ const Navbar = () => (
   <nav className="navbar navbar-expand-lg navbar-light bg-light">
     <div className="container-fluid">
       <Link className="navbar-brand" to="/">MyApp</Link>
-      <div>
-        <Link className="nav-link" to="/">Home</Link>
-        <Link className="nav-link" to="/login">Login</Link>
-        <Link className="nav-link" to="/signup">Signup</Link>
-        <Link className="nav-link" to="/dashboard">Dashboard</Link>
+      
+      <div className="d-flex">
+        <Link className="nav-link px-3" to="/">Home</Link>
+        <Link className="nav-link px-3" to="/login">Login</Link>
+        <Link className="nav-link px-3" to="/signup">Signup</Link>
+        <Link className="nav-link px-3" to="/dashboard">Dashboard</Link>
       </div>
     </div>
   </nav>
